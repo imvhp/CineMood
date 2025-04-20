e0f9396e209715a0188c034013c4b83fc6763b8d
lazyyalex <109808347+lazyyalex@users.noreply.github.com>
2025-04-20 21:22:01 +0700

    initial commit

---
+++ cinemood/src/main/java/com/cinemood/CinemoodApplication.java
@@ -0,0 +1,13 @@
+package com.cinemood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemoodApplication.class, args);
+    }
+
+}
